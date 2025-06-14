3340958304c0d0212cdfb21c17eb2490f8af5bb1
Lucas Watkins <114666925+lucas-watkins@users.noreply.github.com>
2025-06-13 23:05:53 -0400

    Sample Response

--- src/GPTApp.cpp
+++ src/GPTApp.cpp
@@ -10,5 +10,21 @@ void GPTApp::init_routes() {
     CROW_ROUTE(app, "/")([] {
         return indexHTML;
     });
+
+    // Big "genius" idea: security through obscurity
+    CROW_ROUTE(app, "/6a187bbedb71b1917f5ea38d7c960a9a/d07ff2070a2d91dbe30e044a9189525e").methods("POST"_method)
+    ([](const crow::request &r) {
+        const auto request_json = crow::json::load(r.body);
+
+        std::cout << "Language: " << request_json["lang"] << " Message: " << request_json["msg"] << '\n';
+
+        crow::json::wvalue response_json;
+
+        // Echo
+        response_json["lang"] = request_json["lang"];
+        response_json["msg"] = request_json["msg"];
+
+        return response_json.dump();
+    });
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,9 @@
 #include <liboai.h>
 
 int main() {
-    /*const std::unique_ptr<WebApp> app {new GPTApp(8080)};
-    WebApp::serve(app);*/
 
-    liboai::OpenAI ai;
-
+    const std::unique_ptr<WebApp> app {new GPTApp(8080)};
+    WebApp::serve(app);
 
     return 0;
 }
